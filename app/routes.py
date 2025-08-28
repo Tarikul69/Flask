c2d1cb51f0a6f0d6595ffc9cb40119e2e2cdc425
Tarikul69 <tarikulabir69@gmail.com>
2025-08-28 14:50:37 +0600

    Template Added

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, render_template, request
 from .models import User
 from .extensions import db
 
@@ -6,7 +6,17 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return jsonify({"message": "Flask app is running!"})
+    return """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flask Template</title>
+</head>
+<body>
+    <center><h2>Welcome to my flask template</h2></center>
+</body>
+</html>"""
 
 @main.route("/users", methods=["POST"])
 def add_user():
@@ -15,3 +25,8 @@ def add_user():
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "User added!"}), 201
+
+@main.route("/user_list", methods=["GET"])
+def users():
+    data = User.query.all()
+    return jsonify({"data": data})
