c54c4cabe1d0fc79db313cedd3471ff35f2ba247
Tarikul69 <tarikulabir69@gmail.com>
2025-08-29 17:49:20 +0600

    Update

--- app/routes.py
+++ app/routes.py
@@ -4,20 +4,10 @@ from .extensions import db
 
 main = Blueprint("main", __name__)
 
-@main.route("/")
+@main.route("/", methods=["GET"])
 def home():
-    return """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flask Template</title>
-</head>
-<body>
-    <center><h2>Welcome to my flask template</h2></center>
-</body>
-</html>"""
-
+    return render_template("index.html")
+#########################################################
 @main.route("/users", methods=["POST"])
 def add_user():
     data = request.get_json()
@@ -25,7 +15,7 @@ def add_user():
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "User added!"}), 201
-
+#########################################################
 @main.route("/user_list", methods=["GET"])
 def users():
     data = User.query.all()
